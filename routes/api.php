02204472b872b255c8cc6237cd714d947a451a78
sipinto <chairmankmj@gmail.com>
2023-10-05 12:32:20 +0900

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//  Config 파사드 사용
+Route::get('/config',function(){
+    return Config::get('app.maintenance.driver');
+});
+
+// env 메서드 사용
+Route::get('/env', function (){
+    return env();
+});
+
+
