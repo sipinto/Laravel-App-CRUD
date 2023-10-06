4b28b979a7bffa8c8a40f71821363ad814049828
sipinto <chairmankmj@gmail.com>
2023-10-06 23:48:53 +0900

    5 course complete

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>글쓰기</h1>
+    <form action="/articles" method="POST">
+        <input type="text">
+        <input type="button" value="저장하기">
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/articles/create', function (){
+    return view('articles/create');
+});
